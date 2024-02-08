a60a42f3a7ddac7277f31dfe227baf6f47c65652
luka akhaladze <healerluka@gmail.com>
2024-02-08 23:10:59 +0400

    added new account

--- Bankist_inside/script.js
+++ Bankist_inside/script.js
@@ -61,7 +61,27 @@ const account3 = {
   locale: "pt-PT", // de-DE
 };
 
-const accounts = [account1, account2, account3];
+const account4 = {
+  owner: "Lali Ikashvili ",
+  movements: [1000, 90, 90, 120, 10],
+  interestRate: 1.2, // %
+  pin: 7777,
+
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
+  currency: "USD",
+  locale: "pt-PT", // de-DE
+};
+
+const accounts = [account1, account2, account3, account4];
 
 /////////////////////////////////////////////////
 // Elements
